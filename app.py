b91ff120d422758a37541a717200032892bb7d3d
Siddhant Vinit Sukhatankar <43003941+sidvsukhi@users.noreply.github.com>
2020-06-29 23:40:35 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,26 @@
+import pickle
+from flask import Flask, render_template, request
+import numpy as np
+
+#Loading model
+app = Flask(__name__)
+model = pickle.load(open('model.pkl','rb'))
+
+#redirecting API
+@app.route('/')
+def home():
+	return render_template('index.html')
+
+#redirect API to predict
+@app.route('/predict',methods=['POST'])
+def predict():
+	int_features = [int(x) for x in request.form.values()]
+	final_features = [np.array(int_features)]
+	prediction = model.predict(final_features)
+
+	output = round(prediction[0], 2)
+
+	return render_template('index.html', prediction_text='Predicted Employee Salary :${}'.format(output))
+
+if __name__ == "__main__":
+	app.run(debug=True)
